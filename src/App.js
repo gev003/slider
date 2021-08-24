d6fe8caa14baf0082d22749ba0202f806d3f0b80
Gevorg Ghazaryan <gevorg.ghazaryan@gatsoft.am>
2021-08-24 12:49:17 +0400

    final slider

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { FaArrowCircleRight } from "react-icons/fa";
 
-function App() {
+import React from "react";
+
+export default function App() {
+  const [data, setData] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [background, setBackground] = useState("");
+
+  // function fetchingItems() {
+  //   fetch(`https://jsonplaceholder.typicode.com/photos?_limit=5`)
+  //     .then((stream) => stream.json())
+  //     .then((response) => setData(response));
+  // }
+
+  const fetchingItems = async () => {
+    let info = localStorage.getItem("info");
+    if (!info) {
+      const send = await fetch(
+        `https://jsonplaceholder.typicode.com/photos?_limit=5`
+      );
+      const response = await send.json();
+      setData(response);
+      localStorage.setItem("info", JSON.stringify(response));
+    } else {
+      setData(JSON.parse(info));
+    }
+  };
+
+  const incrementTotal = () => {
+    setNumber((prev) => {
+      return prev + 1;
+    });
+  };
+
+  const decrementTotal = () => {
+    setNumber((prev) => {
+      return prev - 1;
+    });
+  };
+
+  useEffect(() => {
+    fetchingItems();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <div className="main">
+          <img
+            src={data.length && data[number].thumbnailUrl}
+            alt="square"
+            onClick={() => {
+              setBackground(
+                `url(${
+                  data.length > 0 && data[number].url
+                }) no-repeat center center`
+              );
+            }}
+          ></img>
+          <span>{data.length && data[number].title}</span>
+          <div>
+            <button
+              type="button"
+              onClick={decrementTotal}
+              disabled={number <= 0}
+            >
+              &#8592;
+            </button>
+            <button
+              type="button"
+              onClick={incrementTotal}
+              disabled={number >= data.length - 1}
+            >
+              &#8594;
+            </button>
+          </div>
+        </div>
+        <div className="divContainer">
+          {data.length &&
+            data.map((obj, index) => (
+              <div
+                className={`unCheckedDev ${
+                  number === obj.id - 1 ? "checked" : ""
+                }`}
+                key={obj.id}
+                onClick={() => setNumber(obj.id - 1)}
+              ></div>
+            ))}
+        </div>
+      </div>
+      <div className="content" style={{ background: `${background}` }}></div>
     </div>
   );
 }
-
-export default App;
